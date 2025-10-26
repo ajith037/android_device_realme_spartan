e2c4aa316c0364a2e9015de0fb1d9172eabc84db
ajith037 <ajith.hsa07@gmail.com>
2025-10-26 23:10:02 +0530

    Revert "spartan: camera: Add enabled bool for finished workarounds"
    This reverts commit b4452cdb0f2273aead6e7cdbb7b88930f625d9db.

--- camera/CameraProviderExtension.cpp
+++ camera/CameraProviderExtension.cpp
@@ -47,7 +47,7 @@ bool supportsSetTorchModeExt() {
 }
 
 int32_t getTorchDefaultStrengthLevelExt() {
-    return 50;
+    return 7;
 }
 
 int32_t getTorchMaxStrengthLevelExt() {
@@ -64,17 +64,17 @@ int32_t getTorchStrengthLevelExt() {
     return get(node, 0);
 }
 
-void setTorchStrengthLevelExt(int32_t torchStrength, bool enabled) {
+void setTorchStrengthLevelExt(int32_t torchStrength) {
     set(TOGGLE_SWITCH, 0);
     for (auto& path : kTorchLedPaths) {
         auto node = path + "/" + TORCH_BRIGHTNESS;
         set(node, torchStrength);
     }
-    if (enabled)
+    if (torchStrength > 0)
         set(TOGGLE_SWITCH, 255);
 }
 
 void setTorchModeExt(bool enabled) {
     int32_t strength = getTorchDefaultStrengthLevelExt();
-    setTorchStrengthLevelExt(enabled ? strength : 0, enabled);
+    setTorchStrengthLevelExt(enabled ? strength : 0);
 }
